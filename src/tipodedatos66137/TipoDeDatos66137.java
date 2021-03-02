3da2b85e38699de776a51cddbe37ef9b5b3b4d63
TRUJEQUE <TRUJEQUE@ANDREA>
2021-03-02 12:04:52 -0600

    datoss

---
+++ src/tipodedatos66137/TipoDeDatos66137.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatos66137;
+
+/**
+ *
+ * @author TRUJEQUE
+ */
+public class TipoDeDatos66137 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("El valor Maximo de byte: " + Byte.MAX_VALUE) ;
+        System.out.println("El valor Minimo de byte: " + Byte.MIN_VALUE) ;
+        System.out.println("El valor Maximo de double: " + Double.MAX_VALUE) ;
+        System.out.println("El valor Minimo de double: " + Double.MIN_VALUE) ;
+        System.out.println("El valor Maximo de short: " + Short.MAX_VALUE) ;
+        System.out.println("El valor Minimo de short: " + Short.MIN_VALUE) ;
+        System.out.println("El valor Maximo de int: " + Integer.MAX_VALUE) ;
+        System.out.println("El valor Minimo de int: " + Integer.MIN_VALUE) ;
+        System.out.println("El valor Maximo de long: " + Long.MAX_VALUE) ;
+        System.out.println("El valor Minimo de long: " + Long.MIN_VALUE) ;
+        
+        
+        
+        
+        
+        
+     
+    }
+    
+}
